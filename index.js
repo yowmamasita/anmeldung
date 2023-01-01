24ba736aa2c71940b180fea56b3f190d669c2cd7
Ben Sarmiento <ben.sarmiento@forto.com>
2023-01-01 19:38:37 +0100

    Fix issues

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ import * as fs from 'fs';
 
 const ANMELDUNG_URL = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=0&anliegen[]=120686&dienstleisterlist=122210,122217,327316,122219,327312,122227,327314,122231,327346,122243,327348,122252,329742,122260,329745,122262,329748,122254,329751,122271,327278,122273,327274,122277,327276,330436,122280,327294,122282,327290,122284,327292,327539,122291,327270,122285,327266,122286,327264,122296,327268,150230,329760,122301,327282,122297,327286,122294,327284,122312,329763,122314,329775,122304,327330,122311,327334,122309,327332,122281,327352,122279,329772,122276,327324,122274,327326,122267,329766,122246,327318,122251,327320,122257,327322,122208,327298,122226,327300,121362,121364&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F120686%2F';
 
-const getDetails = async (browser) => {
+async function getAppointmentSlotsFn(browser) {
+	console.log('getAppointmentSlotsFn');
+
 	const page = await browser.newPage();
 
-	return async (dateUrl) => {
+	console.log('page created');
+
+	return async function innerFn(dateUrl) {
+		console.log('dateUrl', dateUrl);
 		await page.goto(dateUrl);
 
 		const appointmentSlots = await page.evaluate(() => {
@@ -21,9 +26,9 @@ const getDetails = async (browser) => {
 				.map(e => `${appointmentDate} ${e.innerText.replace('\t', ' ')} ${e.querySelector('a').href}`);
 		});
 
-		console.log(appointmentSlots);
+		return appointmentSlots;
 	};
-};
+}
 
 (async () => {
 	const browser = await puppeteer.launch();
@@ -37,14 +42,18 @@ const getDetails = async (browser) => {
 			.map(d => d.href);
 	});
 
-	console.log(bookable);
+	console.log('dates', bookable);
 
-	const appointmentsByDay = await Promise.all(bookable.map(getDetails(browser)));
-	const appointments = appointmentsByDay.flat();
+	if (bookable.length > 0) {
+		const getAppointmentSlots = getAppointmentSlotsFn(browser);
 
-	console.log(appointments);
+		const appointmentsByDay = await Promise.all(bookable.map(async (e) => await getAppointmentSlots(e)));
+		const appointments = appointmentsByDay.flat();
+
+		console.log('appointments', appointments);
+
+		fs.writeFileSync('results.json', appointments.join('\n'));
+	}
 
 	await browser.close();
-
-	fs.writeFileSync('results.json', appointments.join('\n'));
 })();
