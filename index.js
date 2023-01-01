67cb6891bf95ee2f1b3d2092c0479566b98b4b88
Ben Sarmiento <ben.sarmiento@forto.com>
2023-01-01 21:40:10 +0100

    appointment flatten

--- index.js
+++ index.js
@@ -3,31 +3,24 @@ import * as fs from 'fs';
 
 const ANMELDUNG_URL = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=0&anliegen[]=120686&dienstleisterlist=122210,122217,327316,122219,327312,122227,327314,122231,327346,122243,327348,122252,329742,122260,329745,122262,329748,122254,329751,122271,327278,122273,327274,122277,327276,330436,122280,327294,122282,327290,122284,327292,327539,122291,327270,122285,327266,122286,327264,122296,327268,150230,329760,122301,327282,122297,327286,122294,327284,122312,329763,122314,329775,122304,327330,122311,327334,122309,327332,122281,327352,122279,329772,122276,327324,122274,327326,122267,329766,122246,327318,122251,327320,122257,327322,122208,327298,122226,327300,121362,121364&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F120686%2F';
 
-async function getAppointmentSlotsFn(browser) {
-	console.log('getAppointmentSlotsFn');
-
+async function getAppointmentSlots(browser, dateUrl) {
 	const page = await browser.newPage();
 
-	console.log('page created');
+	await page.goto(dateUrl);
 
-	return (async function innerFn(dateUrl) {
-		console.log('dateUrl', dateUrl);
-		await page.goto(dateUrl);
+	const appointmentSlots = await page.evaluate(() => {
+		const parts = window.location.href.split('/');
+		const lastSegment = parts.pop() || parts.pop();
 
-		const appointmentSlots = await page.evaluate(() => {
-			const parts = window.location.href.split('/');
-			const lastSegment = parts.pop() || parts.pop();
+		const appointmentDate = (new Date(parseInt(lastSegment, 10) * 1000))
+			.toLocaleString('de-DE', { dateStyle: 'full', timeZone: 'Europe/Berlin' });
 
-			const appointmentDate = (new Date(parseInt(lastSegment, 10) * 1000))
-				.toLocaleString('de-DE', { dateStyle: 'full', timeZone: 'Europe/Berlin' });
-
-			return Array
-				.from(document.querySelectorAll('tr'))
-				.map(e => `${appointmentDate} ${e.innerText.replace('\t', ' ')} ${e.querySelector('a').href}`);
-		});
-
-		return appointmentSlots;
+		return Array
+			.from(document.querySelectorAll('tr'))
+			.map(e => `${appointmentDate} ${e.innerText.replace('\t', ' ')} ${e.querySelector('a').href}`);
 	});
+
+	return appointmentSlots;
 }
 
 (async () => {
@@ -45,9 +38,7 @@ async function getAppointmentSlotsFn(browser) {
 	console.log('dates', bookable);
 
 	if (bookable.length > 0) {
-		const getAppointmentSlots = getAppointmentSlotsFn(browser);
-
-		const appointmentsByDay = await Promise.all(bookable.map(async (e) => await getAppointmentSlots(e)));
+		const appointmentsByDay = await Promise.all(bookable.map(async (e) => await getAppointmentSlots(browser, e)));
 		const appointments = appointmentsByDay.flat();
 
 		console.log('appointments', appointments);
