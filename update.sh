0c673c03c972cebb2a25383ca4a12bb07531ed59
Ben Sarmiento <ben.sarmiento@forto.com>
2023-01-02 02:31:07 +0100

    Everything, everywhere all at once :shipit:

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git config user.name "automated"
+git config user.email "actions@users.noreply.github.com"
+
+anmeldungUrl="https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=0&anliegen[]=120686&dienstleisterlist=122210,122217,327316,122219,327312,122227,327314,122231,327346,122243,327348,122252,329742,122260,329745,122262,329748,122254,329751,122271,327278,122273,327274,122277,327276,330436,122280,327294,122282,327290,122284,327292,327539,122291,327270,122285,327266,122286,327264,122296,327268,150230,329760,122301,327282,122297,327286,122294,327284,122312,329763,122314,329775,122304,327330,122311,327334,122309,327332,122281,327352,122279,329772,122276,327324,122274,327326,122267,329766,122246,327318,122251,327320,122257,327322,122208,327298,122226,327300,121362,121364&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F120686%2F"
+
+timestamp=$(date -u)
+
+appointmentCount=$(cat results.txt | wc -l)
+
+if [ "${appointmentCount}" -ne "0" ]; then
+	rm -f README.md
+	echo "# list of available Anmeldung appointments as of ${timestamp}\n" > README.md
+	cat results.txt >> README.md
+	curl -d "${appointmentCount} Anmeldung appointment(s) found! Book fast!" -H "Click: ${anmeldungUrl}" ntfy.sh/anmeldung || exit 0;
+fi
+
+git add -A
+git commit -m "latest data: ${timestamp}, slots: ${appointmentCount}" || exit 0
+git push
