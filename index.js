15e79fe28b1ab864ddf683bdd3d0c49d507d753c
automated <actions@users.noreply.github.com>
2023-02-25 12:35:08 +0100

    only do burgeramts near me

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer';
 import * as fs from 'fs';
 import { spawn } from 'child_process';
+import { https } from 'https';
 
 const ANMELDUNG_URL = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen[]=120686&dienstleisterlist=122210,122217,327316,122219,327312,122227,327314,122231,327346,122243,327348,122254,122252,329742,122260,329745,122262,329748,122271,327278,122273,327274,122277,327276,330436,122280,327294,122282,327290,122284,327292,122291,327270,122285,327266,122286,327264,122296,327268,150230,329760,122297,327286,122294,327284,122312,329763,122314,329775,122304,327330,122311,327334,122309,327332,317869,122281,327352,122279,329772,122283,122276,327324,122274,327326,122267,329766,122246,327318,122251,327320,122257,327322,122208,327298,122226,327300&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F120686%2F';
 
@@ -28,7 +29,7 @@ function scrapeAppointments() {
 
 async function appointmentProcessor(browser) {
 	return async (appointment) => {
-		// if (!(/(Schöneweide|Köpenick|Blaschkoallee|Neukölln|Sonnenallee|Zwickauer|Rudow)/.test(appointment))) return;
+		if (!(/(Schöneweide|Köpenick|Blaschkoallee|Neukölln|Sonnenallee|Zwickauer|Rudow)/.test(appointment))) return;
 
 		const urlIndex = appointment.indexOf("https://");
 		const url = appointment.substr(urlIndex);
@@ -101,6 +102,9 @@ async function main(browser) {
 	await page.close();
 
 	if (bookable.length === 0) return;
+
+	await https.request('https://sm.hetrixtools.net/hb/?s=49af524700d95e59166d708d3b1b5359');
+
 	console.log('Found ' + bookable.length + ' bookable dates.', bookable);
 
 	const appointmentsByDay = await Promise.all(bookable.map(async (e) => await getAppointments(browser, e)));
