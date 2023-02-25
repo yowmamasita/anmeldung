e18d62e3e1faf885a139506322508f59ea7c9ca7
Ben Sarmiento <ben.sarmiento@forto.com>
2023-02-25 02:18:13 +0100

    refactor execution schedule

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import puppeteer from 'puppeteer';
 import * as fs from 'fs';
+import { spawn } from 'child_process';
 
 const ANMELDUNG_URL = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen[]=120686&dienstleisterlist=122210,122217,327316,122219,327312,122227,327314,122231,327346,122243,327348,122254,122252,329742,122260,329745,122262,329748,122271,327278,122273,327274,122277,327276,330436,122280,327294,122282,327290,122284,327292,122291,327270,122285,327266,122286,327264,122296,327268,150230,329760,122297,327286,122294,327284,122312,329763,122314,329775,122304,327330,122311,327334,122309,327332,317869,122281,327352,122279,329772,122283,122276,327324,122274,327326,122267,329766,122246,327318,122251,327320,122257,327322,122208,327298,122226,327300&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F120686%2F';
 
@@ -59,7 +60,32 @@ async function getAppointments(browser, dateUrl) {
 	return appointments;
 }
 
-(async () => {
+function executeCommand(command, args) {
+	return new Promise((resolve, reject) => {
+		const process = spawn(command, args);
+
+		let output = '';
+		let error = '';
+
+		process.stdout.on('data', (data) => {
+			output += data.toString();
+		});
+
+		process.stderr.on('data', (data) => {
+			error += data.toString();
+		});
+
+		process.on('close', (code) => {
+			if (code === 0) {
+				resolve(output.trim());
+			} else {
+				reject(new Error(error.trim()));
+			}
+		});
+	});
+}
+
+async function main() {
 	const browser = await puppeteer.launch();
 
 	const page = await browser.newPage();
@@ -92,4 +118,24 @@ async function getAppointments(browser, dateUrl) {
 	}
 
 	await browser.close();
-})();
+
+	try {
+		const output = await executeCommand('update.sh');
+		console.log(output);
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+function runWithTimeout(fn, timeout) {
+	return Promise.race([
+		fn(),
+		new Promise(resolve => setTimeout(() => resolve(), timeout))
+	]);
+}
+
+try {
+	await runWithTimeout(() => { while (true) {main()}}, 59999); // run the function with a 60 second timeout
+} catch (err) {
+	console.log('Finished!')
+}
